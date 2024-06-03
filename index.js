0baaec77305215c3693c9f43d47760d470f59ad4
Felipe Castillo <felipe.rco@gmail.com>
2024-06-02 22:48:37 -0400

    despliegue

--- index.js
+++ index.js
@@ -2,3 +2,30 @@ precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+
+let sumar = document.querySelector("#sumar");
+let restar = document.querySelector("#restar");
+
+sumar.addEventListener("click",()=>{
+    let cantidad = document.querySelector(".cantidad")
+    let valor = cantidad.innerHTML
+    cantidad.innerHTML = parseInt(valor) + 1
+    let total = document.querySelector(".valor-total")
+    total.innerHTML = parseInt(cantidad.innerHTML) * precio;
+
+})
+
+
+restar.addEventListener("click",()=>{
+    let cantidad = document.querySelector(".cantidad")
+    let valor = cantidad.innerHTML
+    cantidad.innerHTML = parseInt(valor) - 1
+    let total = document.querySelector(".valor-total")
+    total.innerHTML = parseInt(cantidad.innerHTML) * precio;
+
+})
+
+
+
+
